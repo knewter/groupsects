50d5de960beef4561ce15d5dc7a7887d8231fce6
Josh Adams <josh@isotope11.com>
2011-05-10 23:23:28 -0500

    attempt using refinery.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupsects::Application.config.secret_token = 'fd7442cf2c37f0cba27920ae757b9c5c4e67215fec2f0add327ec5cf86cd3c92bbf9e27d6568837513e0b7df5c5157dc1585bbbcfd8be90e499a7d17a0fad28a'
